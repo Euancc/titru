f9ca64ce02dafe943f3e541248764ac52b8171b6
Euan Cunningham <euan@Asus-Lap>
2024-05-21 06:12:52 +1100

    footer getting there

--- src/components/jsx/footer.jsx
+++ src/components/jsx/footer.jsx
@@ -5,7 +5,23 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <div className="footer-item"></div>
+        <div className="footer-item">
+          <h3>Contact us</h3>
+          <div className="contact-sect"></div>
+          <p>titiruecolodge@gmail.com</p>
+          <p>7312257</p>
+        </div>
+
+        <div className="footer-item">
+          <h3>Where we are</h3>
+          <div className="location-sect"></div>
+          <p>link to location</p>
+        </div>
+
+        <div className="footer-item">
+          <h3>Connect with us</h3>
+          <div className="conncet-sect"></div>
+        </div>
       </div>
     </footer>
   )
