a9c2c7beb7595637812bc2707fb9f90d0c14220c
Euan Cunningham <euan@Asus-Lap>
2024-06-01 05:54:38 +1100

    form jsx start

--- src/components/jsx/contact.jsx
+++ src/components/jsx/contact.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 import '../css/contact.css'
 
 function Contact() {
-  return <div className="contact-sect">Contact</div>
+  return (
+    <>
+      <section className="contact-sect">
+        <div className="contact-container">
+          <form className="input-fields">
+            <div className="input-title">Name</div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="contact-inputs"
+              required
+            />
+            <div className="input-title">Email</div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Your email"
+              className="contact-inputs"
+              required
+            />
+            <div className="input-title">Message</div>
+            <textarea
+              name="message"
+              placeholder="Your Message"
+              className="contact-inputs"
+              required
+            />
+            <button type="submit">Submit</button>
+          </form>
+
+          <div className="contact-text"></div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Contact
